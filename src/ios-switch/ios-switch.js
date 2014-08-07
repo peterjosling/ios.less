4bca42720b5cc5ecdedb3ec39c0bb65d4af7138e
Peter Josling <peterjosling@gmail.com>
2014-08-07 18:56:59 +0100

    Slide with touch

--- src/ios-switch/ios-switch.js
+++ src/ios-switch/ios-switch.js
@@ -1,11 +1,53 @@
-document.addEventListener('click', function(e) {
-	if (e.target.tagName === 'IOS-SWITCH') {
-		var state = e.target.getAttribute('data-active')
+(function() {
+	var TAG_NAME = 'IOS-SWITCH';
 
-		if (state) {
-			e.target.removeAttribute('data-active')
-		} else {
-			e.target.setAttribute('data-active', true)
+	var initialX,
+		isActive;
+
+	document.addEventListener('click', function(e) {
+		if (e.target.tagName === TAG_NAME) {
+			var state = e.target.getAttribute('data-active');
+
+			if (state) {
+				e.target.removeAttribute('data-active');
+			} else {
+				e.target.setAttribute('data-active', true);
+			}
 		}
-	}
-})
+	})
+
+	document.addEventListener('touchstart', function(e) {
+		if (e.target.tagName === TAG_NAME) {
+			var touch = e.targetTouches[0];
+			initialX = touch.clientX;
+
+			isActive = e.target.getAttribute('data-active')
+
+			e.target.classList.add('active')
+		}
+	})
+
+	document.addEventListener('touchmove', function(e) {
+		if (e.target.tagName === TAG_NAME) {
+			var touch = e.targetTouches[0];
+
+			if  (touch.clientX - initialX > 20 && !isActive) {
+				e.target.setAttribute('data-active', true);
+				isActive = true;
+				initialX = touch.clientX;
+			} else if (touch.clientX - initialX < -20 && isActive) {
+				e.target.removeAttribute('data-active');
+				isActive = false;
+				initialX = touch.clientX;
+			}
+
+			e.preventDefault();
+		}
+	})
+
+	document.addEventListener('touchend', function(e) {
+		if (e.target.tagName === TAG_NAME) {
+			e.target.classList.remove('active')
+		}
+	})
+})();
